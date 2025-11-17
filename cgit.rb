e6bde19272c633d5ad5ac3dec2fac3239e3b661e
Paa Yaw <paayaw.dev@gmail.com>
2025-11-17 11:14:35 +0000

    The first commit

---
+++ cgit.rb
@@ -0,0 +1,71 @@
+#! /usr/bin/env ruby
+require 'byebug'
+
+require 'fileutils'
+require 'pathname'
+require_relative './workspace'
+require_relative './database'
+require_relative './blob'
+require_relative './entry'
+require_relative './tree'
+require_relative './author'
+require_relative './commit'
+
+command = ARGV.shift
+
+case command
+when 'init'
+  path = ARGV.fetch(0, Dir.getwd)
+
+  root_path = Pathname.new(File.expand_path(path))
+  git_path = root_path.join('.git')
+
+  ['objects', 'refs'].each do |dir|
+    begin
+      FileUtils.mkdir_p(git_path.join(dir))
+    rescue Errno::EACCES => error
+      $stderr.puts "fatal: #{error.message}"
+      exit 1
+    end
+  end
+
+  puts "Initialized empty cgit repository in #{git_path}"
+  exit 0
+when 'commit'
+  root_path = Pathname.new(Dir.getwd)
+  git_path = root_path.join(".git")
+  db_path = git_path.join("objects")
+
+  workspace = Workspace.new(root_path)
+  db = Database.new(db_path)
+
+  entries = workspace.list_files.map do |path|
+    data = workspace.read_file(path)
+    blob = Blob.new(data)
+
+    db.store(blob)
+
+    Entry.new(path, blob.oid)
+  end
+
+  tree = Tree.new(entries)
+  db.store(tree)
+
+  name = ENV.fetch('CGIT_AUTHOR_NAME')
+  email = ENV.fetch('CGIT_AUTHOR_EMAIL')
+  author = Author.new(name, email, Time.now)
+  message = $stdin.read
+
+  commit = Commit.new(tree.oid, author, message)
+  db.store(commit)
+
+  File.open(git_path.join('HEAD'), File::WRONLY | File::CREAT) do |file|
+    file.puts(commit.oid)
+  end
+
+  puts "[(root-commit) #{commit.oid}] #{message.lines.first}"
+  exit 0
+else
+  $stderr.puts "cgit: '#{command}' is not cgit command."
+  exit 1
+end
---
+++ workspace.rb
@@ -0,0 +1,17 @@
+require 'byebug'
+
+class Workspace
+  IGNORE = %w[. .. .git]
+
+  def initialize(pathname)
+    @pathname = pathname
+  end
+
+  def list_files
+    Dir.entries(@pathname) - IGNORE
+  end
+
+  def read_file(path)
+    File.read(@pathname.join(path))
+  end
+end
